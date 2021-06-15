794dde89da7ea430dd347670ecb416c07fbf4fa1
Sandro Aparecido Vieira <s.aparecido.vieira@avanade.com>
2021-06-15 09:07:12 -0300

    Open/Close Principle (sOlid) - Implementation of Domain layer (DDD)

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public string Id { get; init; }
+
+        public string RoleName { get; init; }
+    }
+}
