597425e2e1a929d37ba0f66952181d67b07713c7
Sandro Aparecido Vieira <s.aparecido.vieira@avanade.com>
2021-06-17 09:01:05 -0300

    Class 4 - soLid: adding IEmployeeRoleService

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
 {
     public record EmployeeRole
     {
         public EmployeeRole(string id, string roleName)
         {
             Id = id;
+            RoleName = roleName;
+        }
+
+        public EmployeeRole(string roleName)
+        {
             RoleName = roleName;
         }
 
