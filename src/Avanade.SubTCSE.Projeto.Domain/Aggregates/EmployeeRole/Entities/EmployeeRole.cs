a95a9ff97c02cc63362093af30cdaad283cddff8
Sandro Aparecido Vieira <s.aparecido.vieira@avanade.com>
2021-06-23 20:41:13 -0300

    Updating EmployeeRole classes

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -2,12 +2,6 @@
 {
     public record EmployeeRole : BaseEntity<string>
     {
-        public EmployeeRole(string id, string roleName)
-        {
-            Id = id;
-            RoleName = roleName;
-        }
-
         public EmployeeRole(string roleName)
         {
             RoleName = roleName;
