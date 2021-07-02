999101481788e77d46b7ba62930197975075f3af
Sandro Aparecido Vieira <s.aparecido.vieira@avanade.com>
2021-07-01 21:09:07 -0300

    Adding EmployeeValidator class

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Validators/EmployeeValidator.cs
@@ -0,0 +1,25 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Validators;
+using FluentValidation;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Validators
+{
+    public class EmployeeValidator : AbstractValidator<Entities.Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleSet("new", () =>
+            {
+                RuleFor(e => e.FirstName)
+                .NotEmpty()
+                .WithMessage("{PropertyName} cannot be empty");
+
+                RuleFor(e => e.Surname)
+                .NotEmpty()
+                .WithMessage("{PropertyName} cannot be empty");
+
+                RuleFor(e => e.EmployeeRole)
+                .SetValidator(new EmployeeRoleValidator());
+            });
+        }
+    }
+}
